e4c2e955f9bd38d69d9e5297e248ce489306b440
javjm <javjm@outlook.com>
2025-10-24 20:58:51 +0100

    add chromium service import

--- fixtures/ui_fixtures.py
+++ fixtures/ui_fixtures.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, WebDriverException
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromiumService
 import time
 import os
 
